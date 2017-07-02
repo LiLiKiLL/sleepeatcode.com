cbc8e3fda38d097015b572af69dd8f4df4d368ad
chenxu <chenxu@rongyi.com>
2017-07-02 22:08:01 +0800

    完成页面底部

--- resources/views/front/footer.blade.php
+++ resources/views/front/footer.blade.php
@@ -1,5 +1,5 @@
-<div class="row full-width-row">
-    <div class="col-md-12">
+<div class="row" style="background-color: #34343C;color: #ffffff;">
+    <div class="col-md-6 col-md-offset-1">
         <address>
           <h2>Contact Me</h2><br>
           <span>QQ:541817418</span><br>
@@ -7,5 +7,11 @@
           <p>Copyright &copy; 2017 Chenxu. All Rights Reserved.</p>
           <p>赣ICP备16010520号-1</p>
         </address>
+    </div>
+    <div class="col-md-3 pull-right col-md-pull-1" style="padding-top:30px;">
+        <blockquote>
+            如果没有键盘，我的人生就彻底空虚了。
+          <small> <cite title="Source Title">《黑客军团》</cite></small>
+        </blockquote>
     </div>
 </div>
