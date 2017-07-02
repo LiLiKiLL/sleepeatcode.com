328349aaf5979378a2e1970d898e10e5d91dcfa8
chenxu <chenxu@rongyi.com>
2017-07-02 20:00:19 +0800

    头部导航移动设备兼容

--- resources/views/front/header.blade.php
+++ resources/views/front/header.blade.php
@@ -1,19 +1,23 @@
-<div class="row full-width-row" style="padding-bottom:50px;">
-    <div class="col-md-12">
-        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-            <div class="container-fluid">
-                <div class="col-md-10 col-md-offset-1">
-                    <div class="navbar-header">
-                        <a class="navbar-brand" href="{{ route('home')}}">SleepEatCode</a>
-                    </div>
-                    <ul class="nav navbar-nav">
-                        <li><a href="{{ route('home') }}">Home</a></li>
-                        <li><a href="{{ route('blog') }}">Blog</a></li>
-                        <li><a href="{{ route('resume') }}">About</a></li>
-                    </ul>
-                </div>
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation" style="margin-bottom:0px;">
+    <div class="container-fluid">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+                    <span class="sr-only">切换导航</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="{{ route('home')}}">SleepEatCode</a>
             </div>
-        </nav>
-    </div>
-</div>
+            <div class="collapse navbar-collapse" id="navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="{{ route('home') }}">Home</a></li>
+                    <li><a href="{{ route('blog') }}">Blog</a></li>
+                    <li><a href="{{ route('resume') }}">About</a></li>
+                </ul>
+            </div>
 
+        </div>
+    </div>
+</nav>
